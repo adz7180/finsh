1ac9071eb69beed454ccc04d441c6a7b119ba85a
adz7180 <adamhaymour@gmail.com>
2025-06-22 13:16:26 -0600

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <h1 style="color: green;">✔ HexaNest Loaded Successfully</h1>
+    <p>If you see this, routing and rendering are working.</p>
+    <router-link to="/customize">Go to Customize</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
+
+<style scoped>
+.home {
+  padding: 40px;
+  font-family: sans-serif;
+}
+</style>
