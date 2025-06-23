3eeabe2aa41081e23594d9e4f8de38bce1da53a3
adz7180 <adamhaymour@gmail.com>
2025-06-23 14:15:42 -0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   publicPath: '/',
   outputDir: 'dist',
+  productionSourceMap: false,
   chainWebpack: (config) => {
-    config.plugin('html').tap((args) => {
-      args[0].minify = false;
-      return args;
-    });
+    config.plugin('html').tap(args => {
+      args[0].minify = false
+      args[0].inject = 'body' // Ensures scripts load properly
+      return args
+    })
   }
-};
+}
