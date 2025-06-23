7dc4cd25a04b88f447108d4c70712fddaf5c5bb8
adz7180 <adamhaymour@gmail.com>
2025-06-23 14:25:54 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,25 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import errorHandler from './utils/errorHandler'
-import performanceMonitor from './utils/performanceMonitor'
-
-// Polyfills
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
-import './webxr-polyfill'
-
-// Configuration
-Vue.config.productionTip = false
-Vue.use(errorHandler)
-Vue.use(performanceMonitor)
-
+// Remove ALL duplicate Vue imports
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router/Router';
+import store from './store'; // If using Vuex
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store, // Remove if not using Vuex
   render: h => h(App)
 }).$mount('#app');
