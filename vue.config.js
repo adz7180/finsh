6071fadc7ef19a2f2d5ef36b5ef8fdc8288bfc37
adz7180 <adamhaymour@gmail.com>
2025-06-23 15:09:30 -0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,23 @@
 module.exports = {
   publicPath: '/',
-  outputDir: 'dist',
-  productionSourceMap: false,
-  chainWebpack: (config) => {
+  transpileDependencies: true, // Fixes legacy deps
+  chainWebpack: config => {
+    config.resolve.symlinks(false)
     config.plugin('html').tap(args => {
       args[0].minify = false
-      args[0].inject = 'body' // Ensures scripts load properly
+      args[0].inject = 'body'
       return args
     })
+  },
+  configureWebpack: {
+    performance: {
+      hints: false
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        maxSize: 244 * 1024 // 244KB chunks
+      }
+    }
   }
 }
